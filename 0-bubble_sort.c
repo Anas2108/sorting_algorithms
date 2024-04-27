3e53ee941961381d6a4a38086ffee60ab62b64fb
ahmdgm <ahmedgamalgamal1@gmail.com>
2024-04-28 01:00:42 +0300

    change

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -27,13 +27,14 @@ void swap(int *num1, int *num2)
 void bubble_sort(int *array, size_t size)
 {
     int swapped = 1;
+    size_t i, j;
 
     if (!array || !size)
         return;
-    for (size_t i = 0; i < size - 1; i++)
+    for (i = 0; i < size - 1; i++)
     {
         swapped = 0;
-        for (size_t j = 0; j < size - i - 1; j++)
+        for (j = 0; j < size - i - 1; j++)
         {  
            if (array[j] > array[j + 1])
            {
