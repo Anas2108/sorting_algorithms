8fa3edbaddcd9801eb10c08f78db97edf4eb322a
Anas2108 <anashamed12p@gmail.com>
2024-04-27 14:38:16 -0700

    Add sort.h

---
+++ sort.h
@@ -0,0 +1,13 @@
+#ifndef SORT_H
+#define SORT_H
+
+#include <stdlib.h>
+
+void bubble_sort(int *array, size_t size);
+listint_t *swap_node(listint_t *node, listint_t **list);
+void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+
+
+#endif
