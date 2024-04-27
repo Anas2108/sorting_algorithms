3a3adeb572a6d8b702c3d5ceb29c8d4f9366779f
ahmdgm <ahmedgamalgamal1@gmail.com>
2024-04-28 00:55:17 +0300

    change

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -26,22 +26,22 @@ void swap(int *num1, int *num2)
  */
 void bubble_sort(int *array, size_t size)
 {
-    int swapped = true;
+    int swapped = 1;
 
     if (!array || !size)
         return;
     for (size_t i = 0; i < size - 1; i++)
     {
-        swapped = false;
+        swapped = 0;
         for (size_t j = 0; j < size - i - 1; j++)
         {  
            if (array[j] > array[j + 1])
            {
-           swap(&array[j],&array[j + 1]);
+           swap(&array[j], &array[j + 1]);
            }
            
         }
-        if (swapped == false)
+        if (swapped == 0)
         break;
         
     }    
--- sort.h
+++ sort.h
@@ -2,9 +2,16 @@
 #define SORT_H
 
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <limits.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 void bubble_sort(int *array, size_t size);
-listint_t *swap_node(listint_t *node, listint_t **list);
+void swap(int *num1, int *num2);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
